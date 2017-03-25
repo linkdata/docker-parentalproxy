c2694ace2101dda405671ec5b971c8ffc9408673
Johan Lindh <johan@linkdata.se>
2017-03-25 14:26:09 +0100

    fix spelling of blacklist.tar.gz

--- squidGuard.sh
+++ squidGuard.sh
@@ -5,9 +5,9 @@ mkdir -p ${SQUIDGUARD_DB_DIR}
 sed -i "s#http://localhost/block.html#${SQUIDGUARD_BLOCK_URL}#" /etc/squidguard/squidGuard.conf
 
 if [  "${SQUIDGUARD_BLACKLIST_URL}" != "" ]; then
-	sudo wget -O backlist.tar.gz ${SQUIDGUARD_BLACKLIST_URL} \
- 		&& tar -xzf backlist.tar.gz -C /var/lib/squidguard/db \
- 		&& rm backlist.tar.gz \
+	sudo wget -O blacklist.tar.gz ${SQUIDGUARD_BLACKLIST_URL} \
+ 		&& tar -xzf blacklist.tar.gz -C /var/lib/squidguard/db \
+ 		&& rm blacklist.tar.gz \
 		&& squidGuard -C all
 fi
 
